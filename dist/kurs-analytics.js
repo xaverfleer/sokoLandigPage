55e0b051027973572b4ffec98398faa6b215d591
Xaver Fleer <xaver.fleer@gmail.com>
2020-02-23 21:52:59 +0100

    Move kurs-analytics into separate file

---
+++ dist/kurs-analytics.js
@@ -0,0 +1,43 @@
+amplitude.getInstance().logEvent("Kurs page loaded");
+
+var overlay = document.querySelector(".overlay");
+
+document.querySelectorAll(".cta00").forEach(function(e) {
+  e.addEventListener("click", function() {
+    overlay.classList.remove("hidden");
+    amplitude.getInstance().logEvent("Anmelden clicked");
+  });
+});
+
+var observerOptions = { childList: true, attributes: false, subtree: true };
+
+var observer = new MutationObserver(bindVideoEventHandlers);
+var targetNode = document.querySelector("main");
+observer.observe(targetNode, observerOptions);
+
+function bindVideoEventHandlers(mutations) {
+  var addedVideos = mutations.reduce(function(acc, mutation) {
+    Array.prototype.slice
+      .call(mutation.addedNodes)
+      .filter(node => node.nodeName === "VIDEO")
+      .forEach(node => acc.push(node));
+    return acc;
+  }, []);
+
+  addedVideos.forEach(function(video) {
+    var id = video.closest(".video-wrapper").id;
+    video.addEventListener("play", function(event) {
+      amplitude.getInstance().logEvent("Played " + id);
+    });
+    video.addEventListener("ended", function(event) {
+      amplitude.getInstance().logEvent("Ended " + id);
+    });
+    video.addEventListener("pause", function(event) {
+      amplitude.getInstance().logEvent("Paused " + id);
+    });
+  });
+}
+
+document
+  .getElementById("overlay__close-button")
+  .addEventListener("click", e => overlay.classList.add("hidden"));
