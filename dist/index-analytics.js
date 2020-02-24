b2e5a23690b9b10b99d7d4f7bab0371119ef11c0
Xaver Fleer <xaver.fleer@gmail.com>
2020-02-24 17:06:44 +0100

    Use amplitude for tracking anmeldungen

--- dist/index-analytics.js
+++ dist/index-analytics.js
@@ -1,16 +1,29 @@
-amplitude.getInstance().logEvent("Page loaded");
+var amplitudeInstance = amplitude.getInstance();
+amplitudeInstance.logEvent("Page loaded");
 
 var overlay = document.querySelector(".overlay");
 
 document.querySelectorAll(".cta00").forEach(function(e) {
   e.addEventListener("click", function() {
     overlay.classList.remove("hidden");
-    amplitude.getInstance().logEvent("Anmelden clicked");
+    amplitudeInstance.logEvent("Anmelden clicked");
   });
 });
 document.querySelectorAll(".cta01").forEach(function(e) {
   e.addEventListener("click", function() {
-    amplitude.getInstance().logEvent("Zu den Videos clicked");
+    amplitudeInstance.logEvent("Zu den Videos clicked");
+  });
+});
+document.querySelectorAll(".cta02").forEach(function(e) {
+  e.addEventListener("submit", function(e) {
+    e.preventDefault();
+    var fields = [].slice.call(e.target.elements);
+    data = fields.slice(2).reduce(function(acc, elem) {
+      acc[elem.name] = elem.value;
+      return acc;
+    }, {});
+    data.reservations = fields[1].checked ? 2 : 1;
+    amplitudeInstance.logEvent("Absenden submitted", data);
   });
 });
 
