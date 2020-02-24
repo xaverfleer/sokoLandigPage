788514df6fe8a518f1a130ac8712a5cd717e74ab
Xaver Fleer <xaver.fleer@gmail.com>
2020-02-24 17:33:55 +0100

    Track `Absenden submitted`

--- dist/index-analytics.js
+++ dist/index-analytics.js
@@ -1,29 +1,28 @@
-var amplitudeInstance = amplitude.getInstance();
-amplitudeInstance.logEvent("Page loaded");
+amplitude.getInstance.logEvent("Page loaded");
 
 var overlay = document.querySelector(".overlay");
 
 document.querySelectorAll(".cta00").forEach(function(e) {
   e.addEventListener("click", function() {
     overlay.classList.remove("hidden");
-    amplitudeInstance.logEvent("Anmelden clicked");
+    amplitude.getInstance.logEvent("Anmelden clicked");
   });
 });
 document.querySelectorAll(".cta01").forEach(function(e) {
   e.addEventListener("click", function() {
-    amplitudeInstance.logEvent("Zu den Videos clicked");
+    amplitude.getInstance.logEvent("Zu den Videos clicked");
   });
 });
 document.querySelectorAll(".cta02").forEach(function(e) {
   e.addEventListener("submit", function(e) {
     e.preventDefault();
     var fields = [].slice.call(e.target.elements);
-    data = fields.slice(2).reduce(function(acc, elem) {
+    var data = fields.slice(2).reduce(function(acc, elem) {
       acc[elem.name] = elem.value;
       return acc;
     }, {});
     data.reservations = fields[1].checked ? 2 : 1;
-    amplitudeInstance.logEvent("Absenden submitted", data);
+    amplitude.getInstance().logEvent("Absenden submitted", data);
   });
 });
 
