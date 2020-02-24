8641764d65d57bd2cb6d6553c5a986ded9d6aea0
Xaver Fleer <xaver.fleer@gmail.com>
2020-02-24 18:04:27 +0100

    Fix analytics

--- dist/index-analytics.js
+++ dist/index-analytics.js
@@ -1,16 +1,16 @@
-amplitude.getInstance.logEvent("Page loaded");
+amplitude.getInstance().logEvent("Page loaded");
 
 var overlay = document.querySelector(".overlay");
 
 document.querySelectorAll(".cta00").forEach(function(e) {
   e.addEventListener("click", function() {
     overlay.classList.remove("hidden");
-    amplitude.getInstance.logEvent("Anmelden clicked");
+    amplitude.getInstance().logEvent("Anmelden clicked");
   });
 });
 document.querySelectorAll(".cta01").forEach(function(e) {
   e.addEventListener("click", function() {
-    amplitude.getInstance.logEvent("Zu den Videos clicked");
+    amplitude.getInstance().logEvent("Zu den Videos clicked");
   });
 });
 document.querySelectorAll(".cta02").forEach(function(e) {
