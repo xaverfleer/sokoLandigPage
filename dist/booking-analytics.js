42cb980740443aefbbe9531a816af2dff85f0b6a
Xaver Fleer <xaver.fleer@gmail.com>
2020-04-09 16:43:43 +0200

    Add booking.html

---
+++ dist/booking-analytics.js
@@ -0,0 +1,53 @@
+amplitude.getInstance().logEvent("Booking page loaded");
+
+var overlay = document.querySelector(".overlay");
+
+document.querySelectorAll(".cta02").forEach(function(element) {
+  element.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var data = getData(event.target.elements);
+    submitForm(data);
+    element.reset();
+
+    amplitude.getInstance().logEvent("Absenden submitted");
+  });
+});
+
+document
+  .getElementById("overlay__close-button")
+  .addEventListener("click", e => overlay.classList.add("hidden"));
+
+function submitForm(data) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", ".netlify/functions/absenden");
+  xhr.send(data);
+
+  xhr.addEventListener("load", function() {
+    window.location.href = "/submitted.html";
+  });
+
+  xhr.addEventListener("error", xhrEventError => {
+    window.alert(
+      `Senden fehlgeschlagen, bitte erneut versuchen.\n\nBei wiederholtem Fehlschlagen, kontaktiere uns bitte mit folgenden Details: xhrEventError ${JSON.stringify(
+        xhrEventError
+      )}`
+    );
+    window.location.href = "/submission-error.html";
+  });
+}
+
+function getData(elements) {
+  var fields = [].slice.call(elements);
+
+  var raw = fields.slice(2).reduce(function(acc, elem) {
+    acc[elem.name] = elem.value;
+    return acc;
+  }, {});
+  raw.reservations = fields[1].checked ? 2 : 1;
+
+  var stringified = JSON.stringify(raw);
+  var encoded = encodeURIComponent(stringified);
+
+  return encoded;
+}
