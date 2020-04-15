8b55de66e483f11b3da45e26c86f1f4ab0970112
Xaver Fleer <xaver.fleer@gmail.com>
2020-04-15 19:41:14 +0200

    Toggle menu bars for narrow screens

--- dist/index-analytics.js
+++ dist/index-analytics.js
@@ -63,3 +63,11 @@ function getData(elements) {
 
   return encoded;
 }
+
+var nav = document.querySelector(".nav");
+nav.addEventListener("click", function() {
+  var classList = nav.classList;
+  [].slice.call(classList).indexOf("nav--active") > -1
+    ? classList.remove("nav--active")
+    : classList.add("nav--active");
+});
