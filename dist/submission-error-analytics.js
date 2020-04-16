cb91e11c7377e42d6d71218a09fed1cd6f8b64e3
Xaver Fleer <xaver.fleer@gmail.com>
2020-04-16 18:46:35 +0200

    Clean up cta's

--- dist/submission-error-analytics.js
+++ dist/submission-error-analytics.js
@@ -1,11 +1,5 @@
 amplitude.getInstance().logEvent("Submission-error page loaded");
 
-document.querySelectorAll(".cta03").forEach(function(e) {
-  e.addEventListener("click", function() {
-    amplitude.getInstance().logEvent("Zur Startseite");
-  });
-});
-
 var headerContent = document.querySelector(".header__content");
 window.addEventListener("scroll", function(event) {
   window.pageYOffset > 360
--- dist/submitted-analytics.js
+++ dist/submitted-analytics.js
@@ -1,16 +1,5 @@
 amplitude.getInstance().logEvent("Submitted page loaded");
 
-document.querySelectorAll(".cta01").forEach(function(e) {
-  e.addEventListener("click", function() {
-    amplitude.getInstance().logEvent("Zu den Videos clicked");
-  });
-});
-document.querySelectorAll(".cta03").forEach(function(e) {
-  e.addEventListener("click", function() {
-    amplitude.getInstance().logEvent("Zur Startseite");
-  });
-});
-
 var headerContent = document.querySelector(".header__content");
 window.addEventListener("scroll", function(event) {
   window.pageYOffset > 360
