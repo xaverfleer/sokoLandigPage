9bd2098e2bb7b7aa970af8d7fef3f9cea48cf3b8
Xaver Fleer <xaver.fleer@gmail.com>
2020-04-16 17:06:54 +0200

    Fix sticky header for all pages

--- dist/booking-analytics.js
+++ dist/booking-analytics.js
@@ -59,3 +59,10 @@ nav.addEventListener("click", function() {
     ? classList.remove("nav--active")
     : classList.add("nav--active");
 });
+
+var headerContent = document.querySelector(".header__content");
+window.addEventListener("scroll", function(event) {
+  window.pageYOffset > 360
+    ? headerContent.classList.add("header__content--fixed")
+    : headerContent.classList.remove("header__content--fixed");
+});
--- dist/kurs-analytics.js
+++ dist/kurs-analytics.js
@@ -97,3 +97,10 @@ nav.addEventListener("click", function() {
     ? classList.remove("nav--active")
     : classList.add("nav--active");
 });
+
+var headerContent = document.querySelector(".header__content");
+window.addEventListener("scroll", function(event) {
+  window.pageYOffset > 360
+    ? headerContent.classList.add("header__content--fixed")
+    : headerContent.classList.remove("header__content--fixed");
+});
