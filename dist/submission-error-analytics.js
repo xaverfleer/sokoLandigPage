c6af55d180af04e267a889b5636695f46e19209c
Xaver Fleer <xaver.fleer@gmail.com>
2020-04-16 17:07:18 +0200

    Formatting

--- dist/submission-error-analytics.js
+++ dist/submission-error-analytics.js
@@ -1,9 +1,9 @@
 amplitude.getInstance().logEvent("Submission-error page loaded");
 
 document.querySelectorAll(".cta03").forEach(function(e) {
-e.addEventListener("click", function() {
-amplitude.getInstance().logEvent("Zur Startseite");
-});
+  e.addEventListener("click", function() {
+    amplitude.getInstance().logEvent("Zur Startseite");
+  });
 });
 
 var headerContent = document.querySelector(".header__content");
