0927c93826e2c7605b589354c4c03f20b29a4044
Xaver Fleer <xaver.fleer@gmail.com>
2020-04-26 16:29:29 +0200

    Import CSS

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,12 @@ module.exports = {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
