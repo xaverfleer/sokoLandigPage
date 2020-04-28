bad741a8ebeddf074a5c26405540014dca51f8dd
Xaver Fleer <xaver.fleer@gmail.com>
2020-04-28 20:40:42 +0200

    Add webpack basic setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  mode: "development",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(gif|jpg|png|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+};
