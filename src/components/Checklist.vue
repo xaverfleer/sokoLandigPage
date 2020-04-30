b3afe445d16b6c642ec0793e5aaa8be644090d61
Xaver Fleer <xaver.fleer@gmail.com>
2020-04-30 18:26:09 +0200

    Add Checklist component

---
+++ src/components/Checklist.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="checklist">
+    <div class="checklist__item">
+      <div class="checklist__text">
+        …verbindest du dich mit deinem Baby, in dem du dir überlegst, was du ihm
+        von Herzen für sein Leben wünschst&nbsp;(Video&nbsp;1.1)
+      </div>
+    </div>
+    <div class="checklist__item">
+      <div class="checklist__text">
+        …erfährst du, was dein Baby braucht, um innerlich stark zu
+        werden&nbsp;(Video&nbsp;2.2)
+      </div>
+    </div>
+    <div class="checklist__item">
+      <div class="checklist__text">
+        …erklären wir, was es bedeutet, das Baby als gleichwürdiges Subjekt zu
+        sehen&nbsp;(Video&nbsp;2.3)
+      </div>
+    </div>
+    <div class="checklist__item">
+      <div class="checklist__text">
+        …zeigen wir anhand eines Praxisbeispiels auf, was subjektorientierte
+        Kommunikation in der Praxis heissen kann&nbsp;(Video&nbsp;2.4)
+      </div>
+    </div>
+    <div class="checklist__item">
+      <div class="checklist__text">
+        …lernst du anhand von konkreten Sätzen den Unterschied zwischen einer
+        gleichwürdigen und einer herablassenden Sprache
+        kennen&nbsp;(Video&nbsp;2.5)
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
