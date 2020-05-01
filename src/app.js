82a315e455742c99cf5ff0da37b849872d205a27
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-01 22:22:49 +0200

    Move block01 to Block01.vue

--- src/app.js
+++ src/app.js
@@ -1,34 +1,14 @@
 import Vue from "vue/dist/vue.esm";
 import appData from "./appData";
-import Section11 from "./components/Section-1-1.vue";
-import Section12 from "./components/Section-1-2.vue";
-import Section13 from "./components/Section-1-3.vue";
-import Section14 from "./components/Section-1-4.vue";
-import Section15 from "./components/Section-1-5.vue";
-import Section16 from "./components/Section-1-6.vue";
-import Section17 from "./components/Section-1-7.vue";
+import Block1 from "./components/Block-1.vue";
 
 // eslint-disable-next-line no-new
 new Vue({
   template: `
-  <div class="hidden-by-default section--kurs01">
-    <Section-1-1 :documents="documents" :block01="block01"/>
-    <Section-1-2 :documents="documents"/>
-    <Section-1-3 :documents="documents"/>
-    <Section-1-4 :documents="documents"/>
-    <Section-1-5 :documents="documents"/>
-    <Section-1-6 :documents="documents"/>
-    <Section-1-7 :documents="documents"/>
-  </div>`,
+  <Block1 :appData="appData"/>`,
   el: "#app",
-  data: appData,
+  data: { appData },
   components: {
-    Section11,
-    Section12,
-    Section13,
-    Section14,
-    Section15,
-    Section16,
-    Section17,
+    Block1,
   },
 });
---
+++ src/components/Block-1.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hidden-by-default section--kurs01">
+    <Section-1-1 :documents="appData.documents" :block01="appData.block01" />
+    <Section-1-2 :documents="appData.documents" />
+    <Section-1-3 />
+    <Section-1-4 />
+    <Section-1-5 />
+    <Section-1-6 />
+    <Section-1-7 />
+  </div>
+</template>
+
+<script>
+import Section11 from "./Section-1-1.vue";
+import Section12 from "./Section-1-2.vue";
+import Section13 from "./Section-1-3.vue";
+import Section14 from "./Section-1-4.vue";
+import Section15 from "./Section-1-5.vue";
+import Section16 from "./Section-1-6.vue";
+import Section17 from "./Section-1-7.vue";
+
+export default {
+  props: ["appData"],
+  components: {
+    Section11,
+    Section12,
+    Section13,
+    Section14,
+    Section15,
+    Section16,
+    Section17,
+  },
+};
+</script>
+
+<style scoped></style>
