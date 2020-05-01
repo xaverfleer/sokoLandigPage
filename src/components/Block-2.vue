3ea190177360c357465d67217d6551bc34cc3687
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-01 22:41:40 +0200

    Create Block-[1-5].vue

---
+++ src/components/Block-2.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hidden-by-default section--kurs02">
+    <section class="section">
+      <div class="section__subsection">
+        <div class="block__supertitle">Kursblock&nbsp;2</div>
+        <h1 class="block__title">Mit dem Baby interagieren...</h1>
+        <p>In diesem Kursblock…</p>
+        <div class="checklist">
+          <div class="checklist__item">
+            <div class="checklist__text">
+              …verbindest du dich mit deinem Baby, in dem du in drei
+              Einstiegsübungen seine Perspektive einnimmst&nbsp;(Video&nbsp;2.1)
+            </div>
+          </div>
+          <div class="checklist__item">
+            <div class="checklist__text">
+              …lernst du die Objekt- die Geräusch- und die Gebärdensprache als
+              Ergänzung zur gesprochenen Sprache kennen&nbsp;(Video&nbsp;2.2)
+            </div>
+          </div>
+          <div class="checklist__item">
+            <div class="checklist__text">
+              …erfährst du, wie du subjektorientiert mit dem Baby interagieren
+              kannst, wenn du etwas an deinem Baby tust, z.B.
+              wickeln&nbsp;(Video&nbsp;2.3)
+            </div>
+          </div>
+          <div class="checklist__item">
+            <div class="checklist__text">
+              …zeigen wir dir, wie du subjektorientiert mit dem Baby
+              interagieren kannst, wenn du etwas neben deinem Baby
+              tust&nbsp;(Video&nbsp;2.4)
+            </div>
+          </div>
+          <div class="checklist__item">
+            <div class="checklist__text">
+              …zeigen wir auf, wie du subjektorientiert mit dem Baby
+              interagieren kannst, wenn du das Baby
+              verlässt&nbsp;(Video&nbsp;2.5)
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="section__subsection">
+        <p>Bitte melde dich an, um alle Inhalte dieses Kurses zu sehen.</p>
+      </div>
+      <div class="buttons">
+        <a
+          class="button button--primary cta05"
+          href="index.html#prizing"
+          title="Jetzt buchen"
+          >Jetzt buchen</a
+        >
+      </div>
+    </section>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
---
+++ src/components/Block-5.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hidden-by-default section--kurs05">
+    <section class="section">
+      <div class="section__subsection">
+        <div class="block__supertitle">Kursblock&nbsp;5</div>
+        <h1 class="block__title">Zurückhaltung üben...</h1>
+        <p>Bitte melde dich an, um alle Inhalte dieses Kurses zu sehen.</p>
+        <div class="buttons">
+          <a
+            class="button button--primary cta05"
+            href="index.html#prizing"
+            title="Jetzt buchen"
+            >Jetzt buchen</a
+          >
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
