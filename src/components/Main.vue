52460c1f85e07385f2e49eb548b6bda88e9a68aa
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-02 21:09:15 +0200

    Add `Block-2-early-bird.vue`

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,11 @@
 <template>
   <main>
     <Block1 :appData="appData" />
-    <Block2 :appData="appData" />
+    <Block2 :appData="appData" v-if="appData.state.path === 'kurs'" />
+    <Block2EarlyBird
+      :appData="appData"
+      v-if="appData.state.path === 'early-bird'"
+    />
     <Block3 />
     <Block4 />
     <Block5 />
@@ -11,6 +15,7 @@
 <script>
 import Block1 from "./Block-1.vue";
 import Block2 from "./Block-2.vue";
+import Block2EarlyBird from "./Block-2--Early-Bird.vue";
 import Block3 from "./Block-3.vue";
 import Block4 from "./Block-4.vue";
 import Block5 from "./Block-5.vue";
@@ -20,6 +25,7 @@ export default {
   components: {
     Block1,
     Block2,
+    Block2EarlyBird,
     Block3,
     Block4,
     Block5,
