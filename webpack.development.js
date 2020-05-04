723c7dced769264ef4a2178536f2d516caf2f816
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-04 22:40:58 +0200

    Add webpack dev server

--- webpack.development.js
+++ webpack.development.js
@@ -1,8 +1,12 @@
+const path = require("path");
 const commonConfig = require("./webpack.common");
 
 const devConfig = {
   ...commonConfig,
   devtool: "eval-cheap-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "./dist/"),
+  },
   mode: "development",
 };
 
