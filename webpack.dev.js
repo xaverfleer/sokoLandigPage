8f6188493f497e006770b4479b2a17d44c11fc28
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-09 15:03:25 +0200

    Remove not working proxy

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,15 +4,7 @@ const commonConfig = require("./webpack.common");
 const devConfig = {
   ...commonConfig,
   devtool: "eval-cheap-source-map",
-  devServer: {
-    contentBase: path.join(__dirname, "./dist/"),
-    proxy: {
-      "/kurs": {
-        target: "http://localhost:8080/kurs.html",
-        pathRewrite: { "^/kurs$": "" },
-      },
-    },
-  },
+  devServer: { contentBase: path.join(__dirname, "./dist/") },
   mode: "development",
 };
 
