17943240243ced8db42887380a8698e787741b15
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-09 11:54:24 +0200

    Add helpers

--- src/faunaExperiment.js
+++ src/faunaExperiment.js
@@ -6,6 +6,15 @@ const faunadb = require("faunadb");
 const q = faunadb.query;
 const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
 
+const helpers = {
+  handleSuccess(response) {
+    console.log(`success ${response}`);
+  },
+  handleError(error) {
+    console.log(`error: ${error}`);
+  },
+};
+
 // eslint-disable-next-line no-unused-vars
 function createUser(email, password) {
   const salt = crypto.randomBytes(16).toString("base64");
@@ -18,24 +27,16 @@ function createUser(email, password) {
 
   client
     .query(q.Create(q.Collection("users"), { data: payload }))
-    .then(() => {
-      console.log("success");
-    })
-    .catch((error) => {
-      console.log(`error: ${error}`);
-    });
+    .then(helpers.handleSuccess)
+    .catch(helpers.handleError);
 }
 
 // eslint-disable-next-line no-unused-vars
 function getSampleUser() {
   client
     .query(q.Get(q.Match(q.Index("email"), "muster@gmail.com")))
-    .then((response) => {
-      console.log(`success ${response}`);
-    })
-    .catch((error) => {
-      console.log(`error: ${error}`);
-    });
+    .then(helpers.handleSuccess)
+    .catch(helpers.handleError);
 }
 
 // eslint-disable-next-line no-unused-vars
@@ -46,12 +47,9 @@ function createSession(userEmail) {
 
   client
     .query(q.Create(q.Collection("sessions"), { data: payload }))
-    .then((response) => {
-      console.log(`success ${response}`);
-    })
-    .catch((error) => {
-      console.log(`error: ${error}`);
-    });
+    .then(helpers.handleSuccess)
+    .catch(helpers.handleError);
+}
 }
 
 createSession("max@muster.com");
