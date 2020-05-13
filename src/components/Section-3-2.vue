e63055da4f5e1666bf4e21c88c8db57f92aa4831
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-13 23:12:26 +0200

    Add docs and videos

--- src/components/Section-3-2.vue
+++ src/components/Section-3-2.vue
@@ -2,16 +2,21 @@
   <section class="section">
     <div class="section__subsection">
       <SectionTitle :content="section.title" />
+      <VideoVue :video="$root.videos.so3Einstieg" />
+      <Document :document="$root.docs['so3-UebungsdossierPdf']" />
+      <Document :document="$root.docs['so3-UebungsdossierDocx']" />
     </div>
   </section>
 </template>
 
 <script>
+import Document from "./Document.vue";
 import SectionTitle from "./SectionTitle.vue";
+import VideoVue from "./VideoVue.vue";
 
 export default {
   props: ["section"],
-  components: { SectionTitle },
+  components: { SectionTitle, VideoVue, Document },
 };
 </script>
 
