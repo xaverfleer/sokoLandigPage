00ef003733c29359ae406b48a4d4cbc24af45e0a
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-16 12:29:37 +0200

    Lint src/netlify-functions/absenden.js

--- src/netlify-functions/absenden.js
+++ src/netlify-functions/absenden.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // Event format [src](https://docs.netlify.com/functions/build-with-javascript/#format)
 // {
 //     "path": "Path parameter",
@@ -7,34 +8,36 @@
 //     "body": "A JSON string of the request payload."
 //     "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
 // }
-exports.handler = function(event, context, callback) {
+
+const sgMail = require("@sendgrid/mail");
+
+exports.handler = function absenden(event, context, callback) {
   console.log("starting");
 
-  var decoded = decodeURIComponent(event.body);
+  const decoded = decodeURIComponent(event.body);
   console.log(`decoded: ${decoded}`);
 
-  const sgMail = require("@sendgrid/mail");
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
     to: "xaver.fleer+sokokurs@gmail.com",
     from: "noreply@so-kommunizieren.com",
     subject: "Subscription for Early-Bird course",
-    text: decoded
+    text: decoded,
   };
   console.log("sending email");
   sgMail
     .send(msg)
-    .then(function() {
+    .then(function handleSuccess() {
       callback(null, {
         statusCode: 200,
-        body: "Somebody subscribed."
+        body: "Somebody subscribed.",
       });
       console.log("success");
     })
-    .catch(function(e) {
+    .catch(function handleError(e) {
       callback(e, {
         statusCode: 500,
-        body: `failed: + ${e}`
+        body: `failed: + ${e}`,
       });
       console.log("failure");
     });
