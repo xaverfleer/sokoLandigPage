1f086e807b6ffd357d6efe2a36f9cb610d2e113d
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-18 10:37:12 +0200

    Create session

--- src/netlify-functions/login.js
+++ src/netlify-functions/login.js
@@ -26,12 +26,23 @@ const db = {
     );
     return userPromise;
   },
+  createSession(email) {
+    const q = faunadb.query;
+    const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
+    const payload = {
+      sessionId: crypto.randomBytes(16).toString("base64"),
+      email,
+    };
+    const promise = client.query(
+      q.Create(q.Collection("sessions"), { data: payload })
+    );
+    return promise;
+  },
 };
 
 // Event format [src](https://docs.netlify.com/functions/build-with-javascript/#format)
 exports.handler = function register(event, context, callback) {
   console.log("Start login process");
-
   const { email, password } = helpers.parseEventBody(event.body);
   console.log(`Requesting user with email: ${email}`);
   db.fetchUser(email)
@@ -45,6 +56,11 @@ exports.handler = function register(event, context, callback) {
         dbUser.hash
       );
       console.log(`Password is ${correct ? "correct" : "invalid"}`);
+      return correct
+        ? Promise.resolve(dbUser.email)
+        : Promise.reject(new Error("Invalid password"));
+    })
+    .then(db.createSession)
     })
     .catch((e) => {
       console.log(`Error: ${e}`);
