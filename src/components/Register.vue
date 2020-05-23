45199ec7354ea3ed77a816817e3b2457d7777e50
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-23 16:20:24 +0200

    Add register page to the app

---
+++ src/components/Register.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="page">
+    <Header :routes="registerData.routes" />
+    <main>
+      <section class="section section--subscription" id="subscription">
+        <div class="section__subsection">
+          <h2 class="section__header">Konto erstellen</h2>
+          <RegisterForm />
+        </div>
+      </section>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from "./Footer.vue";
+import Header from "./Header.vue";
+import RegisterForm from "./RegisterForm.vue";
+
+export default {
+  components: {
+    Footer,
+    Header,
+    RegisterForm,
+  },
+  computed: {
+    registerData() {
+      return this.$root.appData.register;
+    },
+  },
+};
+
+document.title = `Registrieren | so* kommunizieren`;
+</script>
+
+<style scoped></style>
