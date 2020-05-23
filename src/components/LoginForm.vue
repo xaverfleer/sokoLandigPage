4ee4f4bbe1cc5e6a95a14fdf74a3d8f623c8a5ec
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-23 15:22:12 +0200

    Introduce LoginForm component

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,101 @@
+<template>
+  <form class="form form--narrow" id="login-form">
+    <div class="form-entry form-entry--fullwidth">
+      <label class="form-entry__label text" for="form__email"
+        >E-Mail-Adresse*</label
+      >
+      <input
+        class="form-entry__input text"
+        type="email"
+        name="email"
+        id="form__email"
+        required=""
+      />
+    </div>
+    <div class="form-entry form-entry--fullwidth form-entry--password">
+      <label class="form-entry__label text" for="form__password"
+        >Passwort*</label
+      >
+      <input
+        class="form-entry__input text"
+        type="password"
+        name="password"
+        id="form__password"
+        required=""
+      />
+    </div>
+    <div class="buttons form__buttons form-buttons--dual">
+      <a class="button button--2ndary button--left" href="./register.html"
+        >Konto erstellen</a
+      >
+      <button @click="handleSubmit" class="button button--primary">
+        Anmelden
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleSubmit(event) {
+      const form = this.getForm();
+
+      if (form.checkValidity()) {
+        const data = getData(form.elements);
+        submitForm(data);
+        form.reset();
+      }
+    },
+    getForm() {
+      return document.getElementById("login-form");
+    },
+  },
+};
+
+function getData(elements) {
+  const fields = Array.prototype.slice
+    .call(elements)
+    .filter((elem) => elem.nodeName === "INPUT");
+
+  const raw = fields.reduce(function addField(acc, elem) {
+    acc[elem.name] = elem.value;
+    return acc;
+  }, {});
+
+  const stringified = JSON.stringify(raw);
+  const encoded = encodeURIComponent(stringified);
+
+  return encoded;
+}
+
+function submitForm(data) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("PUT", ".netlify/functions/login");
+  xhr.send(data);
+
+  xhr.addEventListener("load", () => {
+    debugger;
+    switch (xhr.status) {
+      case "504":
+        window.alert(
+          `Anmelden fehlgeschlagen./n/nBitte versuche es später noch einmal oder kontaktiere uns unter kurs@so-kommunizieren.ch`
+        );
+        break;
+      default:
+        const { sessionId } = JSON.parse(decodeURIComponent(xhr.responseText));
+        localStorage.setItem("soko-non-vue", JSON.stringify({ sessionId }));
+    }
+  });
+
+  xhr.addEventListener("error", (xhrEventError) => {
+    window.alert(
+      `Anmelden fehlgeschlagen:/n/nBitte versuche es später noch einmal oder kontaktiere uns unter kurs@so-kommunizieren.ch mit dem dem folgenden Text: xhrEventError ${JSON.stringify(
+        xhrEventError
+      )}`
+    );
+  });
+}
+</script>
+
+<style scoped></style>
