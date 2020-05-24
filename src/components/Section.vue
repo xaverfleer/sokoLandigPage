3cc5c184c642806ad8b6640785af9c18300d0ec9
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-24 15:34:48 +0200

    Styling

--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,22 +1,20 @@
 <template>
   <section class="section">
-    <div class="section__subsection">
-      <template v-if="section.blocktitle">
-        <div class="block__supertitle">{{ section.blocktitle.supertitle }}</div>
-        <h1 class="block__title">{{ section.blocktitle.main }}</h1>
-      </template>
-      <SectionTitle v-if="section.title" :content="section.title" />
-      <VideoVue v-if="section.video" :video="section.video" />
-      <template v-if="section.checklist">
-        <h3>{{ section.subtitle }}</h3>
-        <Checklist :checklist="section.checklist" />
-      </template>
-      <Document
-        v-for="document in section.documents"
-        :document="document"
-        :key="document.href"
-      />
-    </div>
+    <template v-if="section.blocktitle">
+      <div class="block__supertitle">{{ section.blocktitle.supertitle }}</div>
+      <h1 class="block__title">{{ section.blocktitle.main }}</h1>
+    </template>
+    <SectionTitle v-if="section.title" :content="section.title" />
+    <VideoVue v-if="section.video" :video="section.video" />
+    <template v-if="section.checklist">
+      <h3>{{ section.subtitle }}</h3>
+      <Checklist :checklist="section.checklist" />
+    </template>
+    <Document
+      v-for="document in section.documents"
+      :document="document"
+      :key="document.href"
+    />
   </section>
 </template>
 
