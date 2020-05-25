a8f51bfe7089d45454bbaf7a5c78f858bd4eb891
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-25 22:01:50 +0200

    Order logging functions alphabetically

--- src/netlify-functions/private/logging.js
+++ src/netlify-functions/private/logging.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-console */
 module.exports = {
+  log(message) {
+    console.log(message);
+  },
+
   logAndReject(error) {
     console.log(`Error: ${error && error.message}`);
     return Promise.reject(new Error("Failed"));
-  },
-
-  log(message) {
-    console.log(message);
   },
 };
