2208d646cef6a01b88a041640fede9c4ca2d2a88
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-28 12:14:42 +0200

    Add encrypting tests

---
+++ src/netlify-functions/private/cryptingTest.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const test = require("tape");
+const crypting = require("./crypting.js");
+
+test("crypting", (assert) => {
+  test("hash", (assert2) => {
+    const target = crypting.hash;
+    const input = "some input";
+    test("Returns a string", (a) => {
+      a.strictEqual(typeof target(input), "string");
+      a.end();
+    });
+    test("Returned lenght is greater than 10", (a) => {
+      a.strictEqual(target(input).length > 10, true);
+      a.end();
+    });
+    assert2.end();
+  });
+  assert.end();
+});
