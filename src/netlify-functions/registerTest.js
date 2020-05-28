d6bdee91d6349e276c86f405907a11556b040fe8
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-28 11:56:22 +0200

    Fix tests

--- src/netlify-functions/registerTest.js
+++ src/netlify-functions/registerTest.js
@@ -18,11 +18,10 @@ test("helpers.composeUser", (assert) => {
 
   test("Returned object contaoins email, hash and salt", (a) => {
     const returned = target(...sampleInputs);
-    a.strictEqual(returned.email, "dummy@company.com");
-    a.strictEqual(typeof returned.hash, "string");
-    a.strictEqual(typeof returned.salt, "string");
+    a.strictEqual(returned.data.email, "dummy@company.com");
+    a.strictEqual(typeof returned.data.hash, "string");
+    a.strictEqual(typeof returned.data.salt, "string");
     a.end();
   });
   assert.end();
 });
-
