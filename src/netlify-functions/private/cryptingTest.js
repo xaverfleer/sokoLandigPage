32d928df92f7a478c7cb7143bcde663dfa2caad5
Xaver Fleer <xaver.fleer@gmail.com>
2020-05-28 12:19:58 +0200

    Add tests

--- src/netlify-functions/private/cryptingTest.js
+++ src/netlify-functions/private/cryptingTest.js
@@ -16,5 +16,17 @@ test("crypting", (assert) => {
     });
     assert2.end();
   });
+  test("randomString", (assert2) => {
+    const target = crypting.randomString;
+    test("Returns a string", (a) => {
+      a.strictEqual(typeof target(), "string");
+      a.end();
+    });
+    test("Returned lenght is greater than 10", (a) => {
+      a.strictEqual(target().length > 10, true);
+      a.end();
+    });
+    assert2.end();
+  });
   assert.end();
 });
