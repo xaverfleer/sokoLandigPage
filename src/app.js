29000df2377107bb7cd6b4e27362fac2ef2c8d96
Xaver Fleer <xaver.fleer@gmail.com>
2020-06-04 19:03:41 +0200

    Move routes to vueRoutes

--- src/app.js
+++ src/app.js
@@ -3,43 +3,9 @@ import Vue from "vue/dist/vue.esm";
 import VueRouter from "vue-router/dist/vue-router.esm";
 import appData from "./data/appData";
 import stateM8t from "./stateManagement";
-import Account from "./components/Account.vue";
-import AccountDeleted from "./components/AccountDeleted.vue";
-import ConfirmEmail from "./components/ConfirmEmail.vue";
-import Course from "./components/Course.vue";
-import EmailConfirmed from "./components/EmailConfirmed.vue";
-import EmailConfirmFailed from "./components/EmailConfirmFailed.vue";
-import LoggedIn from "./components/LoggedIn.vue";
-import Login from "./components/Login.vue";
-import Register from "./components/Register.vue";
-import RegisterMeeting from "./components/RegisterMeeting.vue";
-import Registered from "./components/Registered.vue";
-import RegisteredMeeting from "./components/RegisteredMeeting.vue";
-import ChangedPassword from "./components/ChangedPassword.vue";
+import routes from "./vueRoutes";
 
 Vue.use(VueRouter);
-
-const routes = [
-  { path: "/account", component: Account },
-  { path: "/account-deleted", component: AccountDeleted },
-  { path: "/changed-password", component: ChangedPassword },
-  { path: "/course/:block", component: Course },
-  { path: "/confirm-email", component: ConfirmEmail },
-  { path: "/email-confirm-failed", component: EmailConfirmFailed },
-  { path: "/email-confirmed", component: EmailConfirmed },
-  { path: "/logged-in", component: LoggedIn },
-  { path: "/login", component: Login },
-  { path: "/register", component: Register },
-  { path: "/register-meeting", component: RegisterMeeting },
-  { path: "/registered", component: Registered },
-  { path: "/registered-meeting", component: RegisteredMeeting },
-  { path: "/kursblock-1", redirect: "course/block-1" },
-  { path: "/kursblock-2", redirect: "course/block-2" },
-  { path: "/kursblock-3", redirect: "course/block-3" },
-  { path: "/kursblock-4", redirect: "course/block-4" },
-  { path: "/kursblock-5", redirect: "course/block-5" },
-  { path: "*", redirect: "course/block-1" },
-];
 
 const router = new VueRouter({ routes });
 
