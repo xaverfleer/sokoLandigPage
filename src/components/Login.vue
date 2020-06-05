3d6dd2ae2e681fa7e61fe854e5defbb309e9e37f
Xaver Fleer <xaver.fleer@gmail.com>
2020-06-05 15:16:46 +0200

    Replace LoginForm.vue with FormVue.vue

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,7 +4,7 @@
     <main>
       <section class="section">
         <h2>Anmelden</h2>
-        <LoginForm />
+        <FormVue :formData="loginForm" />
       </section>
     </main>
     <Footer />
@@ -12,18 +12,39 @@
 </template>
 
 <script>
+import stateManagement from "../stateManagement";
 import Footer from "./Footer.vue";
 import Header from "./Header.vue";
-import LoginForm from "./LoginForm.vue";
+import FormVue from "./FormVue.vue";
 
 export default {
   components: {
     Footer,
     Header,
-    LoginForm,
+    FormVue,
   },
   created() {
     document.title = `Anmelden | so* kommunizieren`;
+  },
+  data() {
+    return {
+      loginForm: {
+        fields: [
+          this.$root.appData.formEntries.email,
+          this.$root.appData.formEntries.password,
+        ],
+        goal: "Anmelden",
+        postSubmit: (xhr) => {
+          const { sessionId, ts } = JSON.parse(
+            decodeURIComponent(xhr.responseText)
+          );
+          stateManagement.updateSession({ sessionId, ts });
+        },
+        secondaryButton: { route: this.$root.appData.routes.register },
+        submitLambdaFunction: "login",
+        successRoute: this.$root.appData.routes.loggedIn.to,
+      },
+    };
   },
 };
 </script>
