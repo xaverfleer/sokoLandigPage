29f233243db5fb7bc375f84daee126269b4ca55f
Xaver Fleer <xaver.fleer@gmail.com>
2020-06-06 11:18:43 +0200

    Replace RegisterForm with FormVue

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,7 +4,7 @@
     <main>
       <section class="section">
         <h2>Konto erstellen</h2>
-        <RegisterForm />
+        <FormVue :formData="registerForm" />
       </section>
     </main>
     <Footer />
@@ -13,17 +13,33 @@
 
 <script>
 import Footer from "./Footer.vue";
+import FormVue from "./FormVue.vue";
 import Header from "./Header.vue";
-import RegisterForm from "./RegisterForm.vue";
 
 export default {
   components: {
     Footer,
+    FormVue,
     Header,
-    RegisterForm,
   },
   created() {
     document.title = `Konto erstellen | so* kommunizieren`;
+  },
+  data() {
+    return {
+      registerForm: {
+        buttonText: "Weiter",
+        fields: [
+          { ...this.$root.appData.formEntries.email, fullWidth: true },
+          this.$root.appData.formEntries.password,
+          this.$root.appData.formEntries.confirm,
+        ],
+        goal: "Konto erstellen",
+        secondaryButton: { route: this.$root.appData.routes.login },
+        submitLambdaFunction: "register",
+        successRoute: this.$root.appData.routes.registered.to,
+      },
+    };
   },
 };
 </script>
