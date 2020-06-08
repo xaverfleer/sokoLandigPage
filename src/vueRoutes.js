ddfe0979b573e940b339512bbe6e72cd2ec31191
Xaver Fleer <xaver.fleer@gmail.com>
2020-06-08 15:50:04 +0200

    Add 404

--- src/vueRoutes.js
+++ src/vueRoutes.js
@@ -11,6 +11,7 @@ import LoggedOut from "./components/LoggedOut.vue";
 import Login from "./components/Login.vue";
 import Logout from "./components/Logout.vue";
 import PasswordLinkSent from "./components/PasswordLinkSent.vue";
+import PageNotFound from "./components/PageNotFound.vue";
 import Register from "./components/Register.vue";
 import RegisterMeeting from "./components/RegisterMeeting.vue";
 import Registered from "./components/Registered.vue";
@@ -31,15 +32,17 @@ export default [
   { path: "/login", component: Login },
   { path: "/logout", component: Logout },
   { path: "/password-link-sent", component: PasswordLinkSent },
+  { path: "/page-not-found", component: PageNotFound },
   { path: "/register", component: Register },
   { path: "/register-meeting", component: RegisterMeeting },
   { path: "/registered", component: Registered },
   { path: "/registered-meeting", component: RegisteredMeeting },
   { path: "/reset-password", component: ResetPassword },
+  { path: "/", redirect: "course/block-1" },
   { path: "/kursblock-1", redirect: "course/block-1" },
   { path: "/kursblock-2", redirect: "course/block-2" },
   { path: "/kursblock-3", redirect: "course/block-3" },
   { path: "/kursblock-4", redirect: "course/block-4" },
   { path: "/kursblock-5", redirect: "course/block-5" },
-  { path: "*", redirect: "course/block-1" },
+  { path: "*", redirect: "page-not-found" },
 ];
