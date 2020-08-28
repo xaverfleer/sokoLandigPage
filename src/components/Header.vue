417c5470b78e6d44b050001f52cb538d30bcc123
Xaver Fleer <xaver.fleer@gmail.com>
2020-08-28 23:13:14 +0200

    Expand header if there is enough space

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
       </a>
       <nav :class="navClasses" @click="toggleNav">
         <NavEntry
-          v-for="route in routes || defaultRoutes"
+          v-for="route in appliedRoutes"
           :key="route.to"
           :route="route"
         />
@@ -32,8 +32,15 @@ import logo from "../imgs/logo.png";
 export default {
   components: { NavEntry },
   computed: {
+    appliedRoutes() {
+      return this.routes || this.defaultRoutes;
+    },
     navClasses() {
-      return ["nav", "nav--kurs", this.isNavActive ? "nav--active" : ""];
+      return [
+        "nav",
+        Object.values(this.appliedRoutes).length > 3 && "nav--kurs",
+        this.isNavActive && "nav--active",
+      ];
     },
     defaultRoutes() {
       return this.$root.appData.standardNavRoutes;
