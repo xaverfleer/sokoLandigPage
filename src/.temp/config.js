3515916f26abca7e05fa0b478644bf00011bf3f4
Xaver Fleer <xaver.fleer@gmail.com>
2021-02-09 14:24:31 +0100

    Do not track generated /src/.temp/

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s | so* kommunizieren",
-  "siteUrl": "",
-  "version": "0.7.23",
-  "catchLinks": true
-}
--- src/.temp/now.js
+++
@@ -1,1 +0,0 @@
-export default 1612700280737
