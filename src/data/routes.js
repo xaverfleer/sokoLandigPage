f55a0a433b16a2583447c2c063e89d6e4c4b5cbb
Xaver Fleer <xaver.fleer@gmail.com>
2021-02-11 00:44:03 +0100

    Add /registered

--- src/data/routes.js
+++ src/data/routes.js
@@ -90,7 +90,7 @@ export default {
   },
   registered: {
     text: "Konto erstellt",
-    to: "/registered",
+    to: "/email-bestaetigen",
   },
   registered2: {
     text: "Link versended",
---
+++ src/pages/EmailBestaetigen.vue
@@ -0,0 +1,35 @@
+<template>
+  <Layout>
+    <section class="section">
+      <h2>E-Mailadresse bestätigen</h2>
+      <p>
+        Wir haben dein Konto erfolgreich erstellt und dir per E-Mail einen Link
+        gesendet. Bitte bestätige deine E-Mail-Adresse mit dem gesendeten Link.
+      </p>
+      <p>
+        <g-link to="/kurs/">Zum Kurs</g-link>
+        oder
+        <g-link href="/">zur Startseite.</g-link>
+      </p>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    meta: [
+      {
+        name: "canonical",
+        href: "https://so-kommunizieren.ch/email-bestaetigen/",
+      },
+      {
+        name: "description",
+        content: "Konto erstellt. Bitte E-Mailadresse bestätigen",
+      },
+      { name: "robots", content: "noindex" },
+    ],
+    title: "E-Mailadresse bestätigen",
+  },
+};
+</script>
