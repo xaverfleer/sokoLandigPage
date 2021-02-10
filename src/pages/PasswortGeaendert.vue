0d6470daa8d4e3c26de09faa51a4478ecc461113
Xaver Fleer <xaver.fleer@gmail.com>
2021-02-10 23:44:41 +0100

    Add /passwort-geaendert/

---
+++ src/pages/PasswortGeaendert.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <section class="section">
+      <h2>Passwort ist erfolgreich geändert</h2>
+      <p>
+        <g-link to="/kurs/">Zum Kurs</g-link>
+        oder
+        <g-link href="/">zur Startseite.</g-link>
+      </p>
+    </section>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    meta: [
+      {
+        name: "canonical",
+        href: "https://so-kommunizieren.ch/passwort-geaendert",
+      },
+      {
+        name: "description",
+        content: "Wir haben dir den Passwortlink per E-Mail zugesendet.",
+      },
+      { name: "robots", content: "noindex" },
+    ],
+    script: [
+      {
+        src: "/amplitudeSnippet.js",
+      },
+    ],
+    title: "Passwort-Link versendet",
+  },
+};
+</script>
