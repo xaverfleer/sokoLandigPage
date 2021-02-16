565561a84619281e48d465db332a2263ff359c86
Xaver Fleer <xaver.fleer@gmail.com>
2021-02-16 18:15:41 +0100

    Add /passwort-definieren/

---
+++ src/pages/PasswortDefinieren.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout>
+    <section class="section">
+      <h2>Passwort definieren</h2>
+      <FormVue :formData="setPasswordForm" />
+    </section>
+  </Layout>
+</template>
+
+<script>
+import FormVue from "~/components/FormVue.vue";
+import appData from "~/data/appData";
+
+export default {
+  components: { FormVue },
+  computed: {
+    setPasswordForm() {
+      return {
+        fields: [appData.formEntries.password, appData.formEntries.confirm],
+        goal: "Passwort definieren",
+        name: "setPassword",
+        submitLambdaFunction: "setInitialPassword",
+        successRoute: appData.routes.definedPassword.to,
+      };
+    },
+  },
+  metaInfo: {
+    meta: [
+      {
+        name: "canonical",
+        href: "https://so-kommunizieren.ch/passwort-definieren/",
+      },
+      {
+        name: "description",
+        content:
+          "Passwort definieren, damit du dich in Zukunft anmelden kannst.",
+      },
+      { name: "robots", content: "noindex" },
+    ],
+    title: "Passwort definieren",
+  },
+};
+</script>
