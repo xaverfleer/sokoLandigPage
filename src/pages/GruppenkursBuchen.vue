41327436bb64306dc222778521e0342dd82d9cdd
Xaver Fleer <xaver.fleer@gmail.com>
2021-02-16 18:07:52 +0100

    Add paypal scripts

--- src/pages/GruppenkursBuchen.vue
+++ src/pages/GruppenkursBuchen.vue
@@ -33,6 +33,16 @@ export default {
           "Geleiteten, strukturierten Onlinekurs buchen und unbeschränkten Zugriff sichern.",
       },
     ],
+    script: [
+      {
+        async: true,
+        src:
+          // Production account
+          "https://www.paypal.com/sdk/js?client-id=AVxs7MSWpOS4Dl88lLb4EPFLK0WVj2x38B69JlPVgtaHXlV5UAiosc_HXefejUgY0a_azV9ktFYclM5E&currency=CHF&integration-date=2020-08-03",
+        // Sandbox account
+        // "https://www.paypal.com/sdk/js?client-id=AU3WQ_fkz9T61156i3E3RGNIg0doaMBqq1FLMEhno702Tth_MZix5a3Dn1LUPsTqNRZ0NoaTobLuRAXE&currency=CHF&integration-date=2020-08-03&debug=true",
+      },
+    ],
     title: "Gruppenkurs buchen",
   },
 };
