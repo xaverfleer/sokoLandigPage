b8d9c3b9f77e1e62efd383246ff595f59ac16426
Xaver Fleer <xaver.fleer@gmail.com>
2021-03-02 11:40:38 +0100

    Dito

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
   extends: ["airbnb-base", "prettier"],
-  ignorePatterns: [
-    "dist/",
-    "node_modules/",
-    "src/assets/",
-    "src-legacy/assets/",
-  ],
+  ignorePatterns: ["dist/", "node_modules/", "src/assets/"],
   rules: {
     "no-underscore-dangle": ["error", { allow: ["__testonly__"] }],
   },
