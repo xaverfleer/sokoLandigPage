09f509de2638f963e9e885c8d1debd41a6314623
Eva Fleer <eva.fleer@gmail.com>
2021-03-05 17:08:30 +0100

    Korrektur 1. Blogeintrag

--- src/pages/Blog/GleichwuerdigOderEntwuerdigend.vue
+++ src/pages/Blog/GleichwuerdigOderEntwuerdigend.vue
@@ -3,25 +3,56 @@
     <Section class="section">
       <p class="blog__date-and-author">22. Februar 2021 – Eva Fleer</p>
       <h1 class="blog__title">Gleichwürdig oder entwürdigend?</h1>
-      <h2>Warum Gleichwürdigkeit?</h2>
       <p>
         Kinder brauchen innere Stärke, um mit den Herausforderungen des Lebens
-        umzugehen und um ihr Leben aktiv zu gestalten.
+        umzugehen und um ihr Leben aktiv zu gestalten. Mit der Art und Weise, wie wir mit Kindern kommunizieren, 
+        können wir Kinder innerlich stärken oder auch schwächen.
       </p>
       <p>
-        Mit der Art und Weise, wie wir mit Kindern kommunizieren, können wir sie
-        innerlich stärken oder auch schwächen. Eine gleichwürdige Sprache stärkt
-        Kinder, eine entwürdigende Sprache schwächt sie. Eine gleichwürdige
+        <strong>Eine gleichwürdige Sprache stärkt
+        Kinder, eine entwürdigende Sprache schwächt sie.</strong>
+      </p>
+      <br />
+      <h2>Gleichwürdige Sprache</h2>
+      <p>Eine gleichwürdige
         Sprache bringt zum Ausdruck, dass wir ein Kind in seiner individuellen
         Persönlichkeit mit seiner ganz eigenen inneren Realität wahr- und
-        ernstnehmen.
+        ernstnehmen. Eine gleichwürdige Sprache vermittelte dem Baby, dass sein Gefühle, 
+        Bedürfnisse und Wünsche gleichermassen wichtig sind, wie die Gefühle, Bedürfnisse 
+        und Wünsche seiner Bezugsperson. Der Alters- und Erfahrungsunterschied zwischen 
+        Erwachsenen und Babys darf nicht dazu führen, einem Baby weniger Würde beizumessen 
+        als einer erwachsenen Person.
       </p>
+      <p>
+        Einem Kind die gleiche Würde wie einer erwachsenen Person zuzugestehen, heisst nicht, dass die 
+        erwachsene Person die Führungsrolle in der Beziehung verliert. Sie trägt nach wie vor die 
+        Hautpverantwortung für das Wohlergehen aller Familienmitglieder und sie trifft auch die nötigen 
+        Entscheide dazu. Es ist klar, dass solche Entscheide manchmal gegen den Willen des Babys getroffen 
+        werden müssen, dann zum Beispiel, wenn das Baby durch eine Handlung die persönliche Grenze seiner 
+        Bezugsperson verletzt. Doch selbst dann ermöglicht eine gleichwürdige Sprache, die Wünsche und 
+        Interessen des Babys als berechtigt (wenn auch nicht erfüllbar) anzuerkennen und dem Baby 
+        zuzugestehen, Gefühle wie Frust, Trauer oder Wut zu empfinden.
+      </p>
+      <p>
+        Eine gleichwürdige Sprache bringt im Kern folendes zum Ausdruck:
+      </p>
+      <p>
+        <strong>Ich sehe und höre dich. Du bist wichtig. Und ich bin auch wichtig. Deswegen kann ich dir 
+          nicht alles ermöglichen und nicht jeden Wunsch erfüllen. Trotzde sind all deine Gedanken, Gefühle, 
+          Bedürfnisse und Wünsche in diesem Moment richtig und berechtigt.</strong>
+      </p>
+      <br />
       <h2>Entwürdigende Sprache</h2>
       <p>
         Leider sind in unserer Gesellschaft entwürdigende, herablassende
-        Aussagen gegenüber Kindern keine Seltenheit. In diesem Video haben wir
-        sieben Kategorien von entwürdigenden Aussagen zusammengestellt. Wir
-        gehen kurz auf gleichwürdige Alternativen ein.
+        Aussagen gegenüber Kindern keine Seltenheit. Viel zu oft werden zum Beispiel Gefühle von Babys 
+        kleingeredet oder geleugnet, unerfüllbare Wünsche als unvernünftig abgetan oder unerwünschte 
+        Verhaltensweisen verständnislos getadelt und bestraft. 
+      </p>
+      <p>
+        In diesem Videsieben habe ich sieben Kategorien von entwürdigenden Aussagen zusammengestellt, denen 
+        ich im Alltag immer wieder begegne. Ich erkläre, warum diese Aussagen entwürdigend sind und ich gehen 
+        darauf ein, welche gleichwürdigen Alternativen wir haben.
       </p>
       <VideoVue :video="{ ref: 'https://player.vimeo.com/video/514884071' }" />
       <p>
