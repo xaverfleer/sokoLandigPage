77ec91dac7fd505679b55c6f678b2321c3e79ef9
Xaver Fleer <xaver.fleer@gmail.com>
2021-03-08 08:02:08 +0100

    Format with Prettier

--- src/pages/Blog/GleichwuerdigOderEntwuerdigend.vue
+++ src/pages/Blog/GleichwuerdigOderEntwuerdigend.vue
@@ -5,99 +5,121 @@
       <h1 class="blog__title">Gleichwürdig oder entwürdigend?</h1>
       <p>
         Kinder brauchen innere Stärke, um mit den Herausforderungen des Lebens
-        umzugehen und um ihr Leben aktiv zu gestalten. Die innere Stärke können wir uns metaphorisch als 
-        einen inneren Baum vorstellen. Die Wurzeln <em>Selbstbewusstsein</em> (ich kenne mich), 
-        <em>Selbstwert</em> (ich bin gut, so wie ich bin) und <em>Sebstvertrauen</em> (ich schaffe das!) 
-        machen diesen inneren Baum stark und widerstandsfähig. Mit der Art und Weise, wie wir mit Kindern kommunizieren, 
-        können wir den inneren Baum stärken oder auch schwächen.
+        umzugehen und um ihr Leben aktiv zu gestalten. Die innere Stärke können
+        wir uns metaphorisch als einen inneren Baum vorstellen. Die Wurzeln
+        <em>Selbstbewusstsein</em> (ich kenne mich), <em>Selbstwert</em> (ich
+        bin gut, so wie ich bin) und <em>Sebstvertrauen</em> (ich schaffe das!)
+        machen diesen inneren Baum stark und widerstandsfähig. Mit der Art und
+        Weise, wie wir mit Kindern kommunizieren, können wir den inneren Baum
+        stärken oder auch schwächen.
       </p>
       <p>
-        <strong>Eine gleichwürdige Sprache stärkt
-        Kinder, eine entwürdigende Sprache schwächt sie.</strong>
+        <strong
+          >Eine gleichwürdige Sprache stärkt Kinder, eine entwürdigende Sprache
+          schwächt sie.</strong
+        >
       </p>
-          <g-image
-            alt="Crying Baby on floor"
-            class="img"
-            src="~/assets/imgs/gleichwuerdig-vs-entwuerdigend.png"
-          />
+      <g-image
+        alt="Crying Baby on floor"
+        class="img"
+        src="~/assets/imgs/gleichwuerdig-vs-entwuerdigend.png"
+      />
       <br /><br />
       <h2>Gleichwürdige Sprache</h2>
-      <p>Eine gleichwürdige
-        Sprache bringt zum Ausdruck, dass wir ein Kind in seiner individuellen
-        Persönlichkeit mit seiner ganz eigenen inneren Realität wahr- und
-        ernstnehmen. Eine gleichwürdige Sprache vermittelte dem Baby, dass seine Gefühle, 
-        Bedürfnisse und Wünsche gleichermassen wichtig und berechtigt sind, wie die Gefühle, Bedürfnisse 
-        und Wünsche seiner Bezugsperson. Der Alters- und Erfahrungsunterschied zwischen 
-        Erwachsenen und Babys darf nicht dazu führen, einem Baby weniger Würde beizumessen 
-        als einer erwachsenen Person.
+      <p>
+        Eine gleichwürdige Sprache bringt zum Ausdruck, dass wir ein Kind in
+        seiner individuellen Persönlichkeit mit seiner ganz eigenen inneren
+        Realität wahr- und ernstnehmen. Eine gleichwürdige Sprache vermittelte
+        dem Baby, dass seine Gefühle, Bedürfnisse und Wünsche gleichermassen
+        wichtig und berechtigt sind, wie die Gefühle, Bedürfnisse und Wünsche
+        seiner Bezugsperson. Der Alters- und Erfahrungsunterschied zwischen
+        Erwachsenen und Babys darf nicht dazu führen, einem Baby weniger Würde
+        beizumessen als einer erwachsenen Person.
       </p>
       <p>
-        Einem Kind die gleiche Würde wie einer erwachsenen Person zuzugestehen, heisst nicht, dass die 
-        erwachsene Person die Führungsrolle in der Beziehung verliert. Sie trägt nach wie vor die 
-        Hautpverantwortung für das Wohlergehen aller Familienmitglieder und sie trifft auch die nötigen 
-        Entscheide dazu. Es ist klar, dass solche Entscheide manchmal gegen den Willen des Babys getroffen 
-        werden müssen, dann zum Beispiel, wenn das Baby durch eine Handlung die persönliche Grenze seiner 
-        Bezugsperson verletzt. Doch selbst dann ermöglicht eine gleichwürdige Sprache, die Wünsche und 
-        Interessen des Babys als berechtigt (wenn auch nicht erfüllbar) anzuerkennen und dem Baby 
-        zuzugestehen, Gefühle wie Frust, Trauer oder Wut zu empfinden.
+        Einem Kind die gleiche Würde wie einer erwachsenen Person zuzugestehen,
+        heisst nicht, dass die erwachsene Person die Führungsrolle in der
+        Beziehung verliert. Sie trägt nach wie vor die Hautpverantwortung für
+        das Wohlergehen aller Familienmitglieder und sie trifft auch die nötigen
+        Entscheide dazu. Es ist klar, dass solche Entscheide manchmal gegen den
+        Willen des Babys getroffen werden müssen, dann zum Beispiel, wenn das
+        Baby durch eine Handlung die persönliche Grenze seiner Bezugsperson
+        verletzt. Doch selbst dann ermöglicht eine gleichwürdige Sprache, die
+        Wünsche und Interessen des Babys als berechtigt (wenn auch nicht
+        erfüllbar) anzuerkennen und dem Baby zuzugestehen, Gefühle wie Frust,
+        Trauer oder Wut zu empfinden.
       </p>
       <p>
         Eine gleichwürdige Sprache bringt im Kern folendes zum Ausdruck:
       </p>
       <p>
-        <strong>Ich sehe und höre dich. Du bist wichtig. Alle hier sind wichtig. Deswegen kann ich dir 
-          nicht alles ermöglichen und nicht jeden Wunsch erfüllen. Trotzdem sind all deine Gedanken, Gefühle, 
-          Bedürfnisse und Wünsche in diesem Moment richtig und berechtigt.</strong>
+        <strong
+          >Ich sehe und höre dich. Du bist wichtig. Alle hier sind wichtig.
+          Deswegen kann ich dir nicht alles ermöglichen und nicht jeden Wunsch
+          erfüllen. Trotzdem sind all deine Gedanken, Gefühle, Bedürfnisse und
+          Wünsche in diesem Moment richtig und berechtigt.</strong
+        >
       </p>
       <p>
-        Ein Beispiel: Dein Baby krabbelt in der Krabbelgruppe zu einem anderen Baby und zieht es unsanft an 
-        den Haaren. Das können wir natürlich nicht zulassen, denn das andere Baby hat ein Recht auf körperliche 
-        Unversehrtheit. Daher musst du die Handlung deines Babys stoppen, etwa durch ein "Stopp, lass die Haare 
-        los!" und wenn nötig durch eine körperliche Grenze, zum Beispiel in dem du deine Hand schützend vor den 
-        Kopf des anderen Babys hältst. Obwohl du dem Baby nicht ermöglichen kannst, seinem Interesse an den Haaren 
-        des anderen Babys nachzugehen, kannst du sein Interesse sehen und Verständnis zeigen. Du kannst 
-        z.B. Folgendes sagen: "Ich verstehe, dass ein solcher Wuschelkopf spannend zum Spielen sein kann. 
-        Aber ich kann nicht zulassen, dass du diesem Kind weh tust." Damit teilst du dem Baby mit, dass mit ihm 
-        und seinen Interessen nichts verkehrt ist, obwohl es ihnen nicht nachgehen kann. Nehmen wir nun an, 
-        dein Baby ist wegen dieser Grenze frustriert und beginnt zu weinen. Obwohl deine Grenze berechtigt ist, 
-        darf dein Baby darüber frustriert sein. Du kannst z.B. sagen: "Du hättest wirklich gerne mit diesen 
-        Haaren gespielt. Ich verstehe, dass du frustriert bist, dass das nicht möglich ist." 
+        Ein Beispiel: Dein Baby krabbelt in der Krabbelgruppe zu einem anderen
+        Baby und zieht es unsanft an den Haaren. Das können wir natürlich nicht
+        zulassen, denn das andere Baby hat ein Recht auf körperliche
+        Unversehrtheit. Daher musst du die Handlung deines Babys stoppen, etwa
+        durch ein "Stopp, lass die Haare los!" und wenn nötig durch eine
+        körperliche Grenze, zum Beispiel in dem du deine Hand schützend vor den
+        Kopf des anderen Babys hältst. Obwohl du dem Baby nicht ermöglichen
+        kannst, seinem Interesse an den Haaren des anderen Babys nachzugehen,
+        kannst du sein Interesse sehen und Verständnis zeigen. Du kannst z.B.
+        Folgendes sagen: "Ich verstehe, dass ein solcher Wuschelkopf spannend
+        zum Spielen sein kann. Aber ich kann nicht zulassen, dass du diesem Kind
+        weh tust." Damit teilst du dem Baby mit, dass mit ihm und seinen
+        Interessen nichts verkehrt ist, obwohl es ihnen nicht nachgehen kann.
+        Nehmen wir nun an, dein Baby ist wegen dieser Grenze frustriert und
+        beginnt zu weinen. Obwohl deine Grenze berechtigt ist, darf dein Baby
+        darüber frustriert sein. Du kannst z.B. sagen: "Du hättest wirklich
+        gerne mit diesen Haaren gespielt. Ich verstehe, dass du frustriert bist,
+        dass das nicht möglich ist."
       </p>
       <br />
       <h2>Entwürdigende Sprache</h2>
       <p>
         Leider sind in unserer Gesellschaft entwürdigende, herablassende
-        Aussagen gegenüber Kindern keine Seltenheit. Viel zu oft werden zum Beispiel Gefühle von Babys 
-        kleingeredet oder geleugnet, unerfüllbare Wünsche als unvernünftig abgetan oder unerwünschte 
-        Verhaltensweisen verständnislos getadelt und bestraft. 
+        Aussagen gegenüber Kindern keine Seltenheit. Viel zu oft werden zum
+        Beispiel Gefühle von Babys kleingeredet oder geleugnet, unerfüllbare
+        Wünsche als unvernünftig abgetan oder unerwünschte Verhaltensweisen
+        verständnislos getadelt und bestraft.
       </p>
       <p>
-        Wenn wir zum Beispiel nochmals das Baby anschauen, dass unsanft an den Haaren eines anderen Babys 
-        zieht, dann könnte eine entwürdigende Aussage lauten: "Was fällt dir ein, du Grobian. Wehe, du tust 
-        das noch einmal!" Das Baby erfährt so, dass mit ihm, seinen Interessen und seiner Art, die Welt 
-        zu erkunden, etwas falsch ist. Vielleicht beginnt es zu weinen und schon hört es die nächste 
-        entwürdigende Aussage: "Jetzt brauchst du gar nicht so ein Theater zu machen!". Nun sind also auch 
-        noch seine Gefühle falsch und unberechtigt. Wirklich entwürdigent!
+        Wenn wir zum Beispiel nochmals das Baby anschauen, dass unsanft an den
+        Haaren eines anderen Babys zieht, dann könnte eine entwürdigende Aussage
+        lauten: "Was fällt dir ein, du Grobian. Wehe, du tust das noch einmal!"
+        Das Baby erfährt so, dass mit ihm, seinen Interessen und seiner Art, die
+        Welt zu erkunden, etwas falsch ist. Vielleicht beginnt es zu weinen und
+        schon hört es die nächste entwürdigende Aussage: "Jetzt brauchst du gar
+        nicht so ein Theater zu machen!". Nun sind also auch noch seine Gefühle
+        falsch und unberechtigt. Wirklich entwürdigent!
       </p>
       <p>
-        In diesem Video habe ich sieben Kategorien von entwürdigenden Aussagen zusammengestellt, denen 
-        ich im Alltag immer wieder begegne. Ich erkläre, warum diese Aussagen entwürdigend sind und ich gehen 
-        kurz darauf ein, welche gleichwürdigen Alternativen wir haben.
+        In diesem Video habe ich sieben Kategorien von entwürdigenden Aussagen
+        zusammengestellt, denen ich im Alltag immer wieder begegne. Ich erkläre,
+        warum diese Aussagen entwürdigend sind und ich gehen kurz darauf ein,
+        welche gleichwürdigen Alternativen wir haben.
       </p>
       <VideoVue :video="{ ref: 'https://player.vimeo.com/video/514884071' }" />
       <div class="buttons">
         <g-link
           class="button button--primary"
-           to="/blog"
-           title="Zurück zur Übersicht"
-           >Zur Übersicht</g-link
-            >
+          to="/blog"
+          title="Zurück zur Übersicht"
+          >Zur Übersicht</g-link
+        >
         <g-link
-          class ="button button--2ndary"
+          class="button button--2ndary"
           to="/kurs/01-innere-staerke/"
           target="_blank"
           title="Zum Onlinekurs"
-          >Zum Onlinekurs</g-link>
+          >Zum Onlinekurs</g-link
+        >
       </div>
     </Section>
   </Layout>
