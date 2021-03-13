fb09aef9654bf11aea80ae9594666ef9ce5a37d3
Eva Fleer <eva.fleer@gmail.com>
2021-03-13 17:39:03 +0100

    Blogeintrag ABC der Zurückhaltung

---
+++ src/pages/Blog/AbcDerZurueckhaltung.vue
@@ -0,0 +1,218 @@
+<template>
+  <Layout>
+    <Section class="section">
+      <p class="blog__date-and-author">13. März 2021 – Eva Fleer</p>
+      <h1 class="blog__title">
+        Das ABC der Zurückhaltung <br />Ein Leitfaden für die Begleitung des
+        kindlichen Spiels.
+      </h1>
+      <p>
+        Der Impuls in das Spiel eines Kindes einzugreifen ist manchmal gross und
+        schwierig zu unterdrücken. Schnell geben wir Ratschläge, lenken das
+        Interesse auf etwas Spannendes, zeigen, wie etwas funktioniert,
+        bewerten, bieten Hilfestellungen an, retten oder lösen ein Problem oder
+        einen Konflikt. Es ist nicht einfach diese Impulse zu kontrollieren,
+        während das Baby spielt und forscht. Doch es gibt gute Gründe, uns darin
+        zu üben, zurückhaltend zu sein.
+      </p>
+      <br />
+      <h2>Sieben Gründe für mehr Zurückhaltung</h2>
+      <p>
+        <strong>Interessensverlust:</strong> Diverse Studien belegen, dass das
+        Interesse eines Kindes an einer Tätigkeit abnimmt, wenn es sich dazu
+        gedrängt fühlt, etwas (auf eine bestimmte Art und Weise) zu tun. Es
+        verliert nicht nur das Interesse an einer bestimmten Aktivität, sondern
+        auch das Interesse daran, Gegenstände und Aktivitäten kreativ und
+        vielfältig zu erkunden.
+      </p>
+      <p>
+        <strong>Leistungsdruck:</strong> Kinder, die beim Spielen angetrieben
+        werden, etwas zu schaffen (z.B. durch Anweisungen oder Lob) bzw. Babys,
+        deren Eltern eine sehr belehrende Rolle einnehmen, neigen später dazu,
+        ihren Selbstwert an ihre Leistung zu knüpfen: Sie fühlen sich nur dann
+        wertvoll und liebeswürdig, wenn sie besondere Leistung erbringen können.
+      </p>
+      <p>
+        <strong>Gefühl der Inkompetenz:</strong> Wenn Eltern dem Kind ständig
+        Dinge beibringen wollen, die das Kind selbst noch nicht kann, nimmt das
+        Vertrauen des Kindes in seine Fähigkeiten ab. Das Kind erlebt dann, dass
+        die Art und Weise, wie es forscht und lernt, nicht gut genug ist. So
+        sinken mit der Zeit das Selbstvertrauen und der Mut, neue Aufgaben
+        anzugehen.
+      </p>
+      <p>
+        <strong>Verlust der Frustrationstoleranz</strong> Kinder, deren Eltern
+        (bei Frust) sehr schnell helfen oder Lösungen anbieten, verlieren mit
+        der Zeit ihre angeborene Frustrationstoleranz. Anstatt Frust in
+        Motivation umzuwandeln und Motivation schliesslich in Stolz, lernen
+        diese Kinder, bei Frust schnell aufzugeben und sich von der Hilfe von
+        aussen abhängig zu machen.
+      </p>
+      <p>
+        <strong>Verminderte Körperwahrnehmung</strong> Kinder, die ständig dazu
+        animiert und dabei unterstützt werden, Dinge zu tun, für die sie noch
+        nicht bereit sind, haben Schwierigkeiten zu lernen, sich auf ihre
+        Selbsteinschätzung und ihr Körpergefühl zu verlassen. So bauen sie weder
+        eine gute Selbstwahrnehmung noch eine gute Selbsteinschätzung auf.
+      </p>
+      <p>
+        <strong>Abhängigkeit</strong> Eine weitere Gefahr von übermässigem
+        Bespielen oder Belehren ist, dass das Kind von den Inputs der Eltern
+        abhängig werden kann. Es verliert seine Spontaneität und
+        Eigeninitiative, weil ungebetene Anweisungen dem Baby die Möglichkeit
+        nehmen, auf seine Weise zu denken, zu entdecken und zu experimentieren.
+      </p>
+      <p>
+        <strong>Reizüberflutung</strong> Beim Entertainen des Kindes besteht
+        auch immer die Gefahr, dass wir das Baby überreizen, wenn wir nicht sehr
+        vorsichtig auf seine Signale achten (z.B. glasige Augen, Abwenden des
+        Blicks, unruhiges Quengeln…). Kinder, die oft überreizt sind, können
+        chronisch ängstlich oder verwirrt sein und wenig Selbstvertrauen in ihre
+        Fähigkeiten entwickeln. Zudem leiden sie auch oft an Schlafproblemen.
+      </p>
+      <br />
+      <h2>Das ABC der Zurückhaltung: <br />Abwarten, Beschreiben, Coachen</h2>
+      <g-image
+        alt="Child building blocks"
+        class="img"
+        src="~/assets/imgs/ABC-Der-Zurueckhaltung.jpg"
+      />
+      <p>
+        Das ABC der Zurückhaltung kann Eltern dabei unterstützen, auf eine
+        zurückhaltende Art und Weise mit den Erkundungsbedürfnissen ihres Kindes
+        umzugehen. Das heisst nicht, dass wir nie mit dem Kind spielen, ihm nie
+        helfen oder etwas zeigen dürfen. Es heisst aber, dass wir in Momenten,
+        in denen das Kind in ein Spiel vertieft ist, eine ermöglichende und
+        Zurück-Haltung einnehmen und nicht vorschnell mit Inputs, Ratschlägen,
+        Lösungen, Belehrungen, Bewertungen, Entertainment, Rettungsmanövern usw.
+        in das Spiel eingreifen.
+      </p>
+      <br />
+      <h2>
+        Schritt 1: Abwarten
+      </h2>
+      <p>
+        In vielen Erkundungssituationen ist es ein grosses Geschenk für unser
+        Kind, wenn wir einfach abwarten und ihm dadurch ermöglichen, seine
+        Umwelt nach seinen eigenen Interesse, in seinem eigenen Tempo und auf
+        seine eigene Art und Weise zu erkunden sowie eigene Lösungen für
+        Probleme zu entwickeln. Solange das Kind von uns keine Inputs und keine
+        Unterstützung einfordert, solange es sich keinen grossen Gefahren
+        aussetzt und so lange es nicht unsere persönlichen Grenzen verletzt,
+        können wir abwarten und uns darüber freuen, wie kreativ und eigenständig
+        unser Kind die Welt entdeckt. Es gibt z.B. keinen Grund, einem Kind zu
+        erklären, wie ein Spielzeug «richtig» funktioniert, wenn es dieses
+        "falsch" anwendet. Es gibt auch keinen Grundm einem Baby zu helfen, sich
+        auf den Bauch zu drehen, wenn es konzentriert dabei ist, das zu üben.
+        Kinder wollen und können «es» selbst schaffen!
+      </p>
+      <br />
+      <h2>
+        Schritt 2: Beschreiben
+      </h2>
+      <p>
+        Wenn das Kind uns voller Stolz etwas zeigt, wenn es immer frustrierter
+        wird, weil etwas nicht klappt, wenn es sich in Gefahr oder in eine
+        missliche Lage bringt oder wenn es in einen Konflikt gerät, dann wollen
+        und können wir irgendwann nicht mehr abwarten. Nun können wir aber in
+        vielen Situationen immer noch zurückhaltend bleiben, indem wir einfach
+        beschreiben, was wir wahrnehmen. Wir geben keine Inputs, Bewertungen
+        oder Ratschläge und bieten keine Lösungen. Alles, was wir tun, ist,
+        Kontakt zu unserem Kind aufzunehmen und ihm seine Situation zu
+        beschreiben, damit es sich besser darin zurechtfinden kann. Beim
+        Beschreiben können wir…:
+      </p>
+      <ul>
+        <li>
+          …die Situation beschreiben: <br />«Du hast dich gerade ganz allein da
+          hochgezogen»
+        </li>
+        <li>
+          …die wahrgenommenen Gefühle des Kindes spiegeln: <br />«Du siehst
+          richtig glücklich und stolz aus!»
+        </li>
+        <li>
+          …unsere eigenen Gefühle zum Ausdruck bringen: <br />«Ich freue mich
+          gerade sehr mit dir!»
+        </li>
+      </ul>
+      <br />
+      <h2>
+        Schritt 3: Coachen
+      </h2>
+      <p>
+        Wenn das Kind sehr frustriert ist, wenn es in einen komplizierten
+        Konflikt gerät, wenn es sich in eine wirklich schwierige Lage manövriert
+        hat usw., dann können wir nach dem Beschreiben dazu übergehen,
+        zurückhaltend zu coachen. Beim Coachen geht es darum, dem Kind zu
+        helfen, ein Problem (mit der Zeit) selbstständig lösen zu können. Wir
+        halten uns also immer noch damit zurück, ein Problem für unser Kind zu
+        lösen. Stattdessen geben wir vielleicht zurückhaltende Hilfestellungen
+        («Manchmal hilft es, wenn…») oder schlagen Alternativen vor («Hier hat
+        es noch eine zweite Schaufel, vielleicht mag jemand von euch damit
+        spielen»). Vielleicht bieten wir Halt («Ich stehe neben dir. Wenn du
+        fällst, kannst du dich an mir festhalten» oder wir erleichtern die
+        Aufgabe etwas («Ich stelle dir ein Podest hin, damit du besser
+        hochsteigen kannst»). Vielleicht fangen wir einen Sturz auf und federn
+        ihn ab, anstatt ihn zu verhindern, so dass unser Kind seine
+        Körperwahrnehmung verfeinern kann.
+      </p>
+      <p>
+        Differenzierte Inputs, wie du das ABC der Zurückhaltung auf verschiedene
+        Situationen anwenden kannst, findest du in unserem Online-Video-Kurs. Im
+        folgenden PDF findest du Ideen zu drei konkreten Beispielen.
+      </p>
+      <div class="document">
+        <a
+          href="/documents/Blog-ABC-der-Zurueckhaltung-Beispiele.pdf"
+          target="_blank"
+          class="document__link document__link--further-information"
+          >Kursdaten und Kursplan</a
+        >
+      </div>
+      <EmailSubscription />
+      <div class="buttons">
+        <g-link
+          class="button button--2ndary"
+          to="/blog"
+          title="Zurück zur Übersicht"
+          >Zur Übersicht</g-link
+        >
+        <g-link
+          class="button button--2ndary"
+          to="/kurs/01-innere-staerke/"
+          target="_blank"
+          title="Zum Onlinekurs"
+          >Zum Onlinekurs</g-link
+        >
+      </div>
+    </Section>
+  </Layout>
+</template>
+
+<script>
+import EmailSubscription from "~/components/EmailSubscription.vue";
+import VideoVue from "~/components/VideoVue.vue";
+import { trackPageLoad } from "~/scripts/analyticsMethods";
+
+export default {
+  components: { EmailSubscription, VideoVue },
+  metaInfo: {
+    meta: [
+      {
+        name: "canonical",
+        href: "https://so-kommunizieren.ch/blog/abc-der-zurueckhaltung/",
+      },
+      {
+        name: "description",
+        content:
+          "Wie können wir den Forscher- und Entdeckerdrang unserer Kinder stärken? Das ABC der Zurückhaltung als Leitfaden",
+      },
+    ],
+    title: "Nein sagen – Drei Strategien.",
+  },
+  mounted() {
+    trackPageLoad(this.$route.path);
+  },
+};
+</script>
