aa4aaa036a406e0c8543062889ddf9e0a9143af9
Xaver Fleer <xaver.fleer@gmail.com>
2021-03-17 15:09:59 +0100

    Add philosophy

---
+++ src/pages/Philosophie.vue
@@ -0,0 +1,28 @@
+<template>
+  <Layout>
+    <Section class="section">
+      <h1>Philosophie der <em>subjektorientierten</em> Kommunikation</h1>
+    </Section>
+  </Layout>
+</template>
+
+<script>
+import { trackPageLoad } from "~/scripts/analyticsMethods";
+
+export default {
+  metaInfo: {
+    meta: [
+      { name: "canonical", href: "https://so-kommunizieren.ch/blog/" },
+      {
+        name: "description",
+        content:
+          "Philosophie – Grundgedanken der subjektorientierten Kommunikation",
+      },
+    ],
+    title: "Philosophie",
+  },
+  mounted() {
+    trackPageLoad(this.$route.path);
+  },
+};
+</script>
