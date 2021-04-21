c9625ac98a6cc10c7696393f017da721f242c892
Eva Fleer <eva.fleer@gmail.com>
2021-04-21 15:19:12 +0200

    Blogeintrag Betreuungswechsel

---
+++ src/pages/Blog/DreiIdeenFuerBetreuungswechselUndTrennungen.vue
@@ -0,0 +1,347 @@
+<template>
+  <Layout>
+    <section>
+      <p class="blog__date-and-author">21. April 2021 – Eva Fleer</p>
+      <h1 class="blog__title">
+        Drei Ideen, um einem Kind Betreuungswechsel und Trennungen zu
+        erleichtern
+      </h1>
+      <p>
+        Der Übergang von einer Betreuungssituation zu einer anderen (z.B. von
+        Eltern- zu Kitazeit oder von Mama- zu Papazeit) fällt vielen Kindern
+        schwer. Manche Kinder weinen und klammern sich beim Übergang, der ja
+        auch immer ein Abschied ist, an ihre (primäre) Bezugsperson.
+      </p>
+      <p>
+        Als Mutter weiss ich, wie schwer solche tränenreichen Übergänge nicht
+        nur für das Kind, sondern auch für uns Eltern sind. Ein weinendes Kind
+        zu verlassen kann ganz schön schmerzen, auch wenn wir wissen, dass es
+        gut aufgehoben ist.
+      </p>
+      <p>
+        In diesem Artikel zeige ich drei Ideen auf, wie wir Eltern unseren
+        Kindern einen Betreuungsübergang erleichtern können. Es ist mir wichtig
+        vorab folgendes klarzustellen: Unsere Kinder dürfen weinen, wenn sie
+        Trennungsschmerz empfinden und wenn ihnen Übergänge zu schaffen machen!
+      </p>
+      <ol>
+        <li>
+          <strong
+            >Abschiedstränen bedeuten nicht, dass wir als Eltern etwas falsch
+            gemacht haben!</strong
+          >
+          <br />Im Gegenteil: Aus Sicht der Bindungstheorie ist es insbesondere
+          gerade bei kleinen Kindern ganz normal und ein Zeichen einer sicheren
+          Bindung, wenn sie bei Trennungssituationen weinen. Es ist kein
+          schlechtes Zeichen, wenn kleine Kinder bei ihren Bindungspersonen sein
+          wollen und sich auch getrauen, dies zu zeigen. Das heisst nun wiederum
+          nicht, dass unsere Bindung zum Kind gestört ist, wenn das Kind nicht
+          weint. Kinder bringen nun mal unterschiedliche Temperamente und
+          Persönlichkeitseigenschaften mit und gehen mit schwierigen Situationen
+          unterschiedlich um.
+        </li>
+        <br />
+        <li>
+          <strong
+            >Abschiedstränen bedeuten auch nicht, dass etwas mit unserem Kind
+            nicht stimmt,</strong
+          >
+          <br />dass es z.B. noch zu unselbständig oder zu schüchtern ist.
+          Leider werden viele Eltern oft mit der Idee konfrontiert, dass es
+          lobenswert ist, wenn ein Kind Trennungssituationen früh einfach
+          wegsteckt. Dabei wird aber oft übersehen, dass manche Kinder auch
+          einfach deswegen nicht weinen, weil sie resigniert haben («es nimmt
+          mich ja sowieso niemand ernst») oder weil sie Angst vor einer
+          negativen Reaktion auf ihr Weinen haben («Wenn ich weine, verliere ich
+          die Liebe/Geduld/Zuneigung meiner Bezugsperson»). Solche Kinder kommen
+          dann zwar vordergründig gut mit Trennungssituationen und
+          Betreuungswechseln klar. Misst man jedoch die Werte des Stresshormons
+          Cortisol, kann man feststellen, dass es ihnen innerlich nicht gut
+          geht. Nur haben sie gelernt, es nicht zu zeigen.
+        </li>
+      </ol>
+      <p>
+        Kinder haben also das Recht, bei einem Abschied bzw. bei einem
+        Betreuungswechsel zu weinen, daran ist nichts verkehrt. Dennoch können
+        wir versuchen, die Übergangssituationen für das Kind etwas einfacher zu
+        gestalten. Keine der folgenden drei Ideen zielt jedoch darauf ab, das
+        Weinen zu vermeiden oder abzuschalten. Der Fokus der Ideen liegt darauf,
+        die Übergänge, Abschiede und Betreuungswechsel vorhersehbar zu machen.
+        <br /><br />
+      </p>
+
+      <h2>Idee Nummer 1: Die Zeit bis zum Wechsel visualisieren</h2>
+      <p>
+        Betreuungswechsel fallen Kindern besonders schwer, wenn sie für sie
+        völlig unvorhersehbar und plötzlich geschehen. Es ist grundsätzlich
+        hilfreich, einen bevorstehenden Wechsel anzukünden. Spezielle Uhren
+        (oder andere Hilfsmittel) können uns dabei dienen, die verbleibende Zeit
+        bis zu einem Betreuungswechsel zu visualisieren.
+      </p>
+      <p>
+        Die Visualisierung der Zeit kann einem Kind helfen, sich auf einen
+        Betreuungswechsel einzustellen. Das Kind hat so eine Orientierung und
+        wird nicht plötzlich von einem Wechsel überrascht. Auch wenn das Kind
+        eine Bezugsperson vermisst und diese in absehbarer Zeit nach Hause
+        kommt, kann ein TimeTimer helfen, das Warten auf die Bezugsperson
+        einfacher zu machen.
+      </p>
+      <p>
+        Wir benutzen zuhause oft einen sogenannten TimeTimer. Auf dem TimeTimer
+        können wir (wie bei einem normalen Timer) einstellen, wie viel Zeit bis
+        zu einem bestimmten Event noch vergeht. Die verbleibende Dauer wird
+        dabei farblich visualisiert: Entsprechend der ausgewählten Zeit wird
+        eine grössere oder kleinere Fläche des Kreises farbig markiert. Mit
+        fortschreitender Zeit wird diese Fläche kleiner, bis sie schliesslich
+        verschwindet, wenn die eingestellte Zeit vorbei ist.
+      </p>
+      <g-image alt="TimeTimer" class="img" src="~/assets/imgs/timetimer2.jpg" />
+      <p>
+        Wir haben zuahsue das Modell
+        <a href="https://shop.muelicom.ch/Time-Timer-MOD" target="_blank">
+          TimeTimer MOD >></a
+        >. Ein sehr nützliches Feature davon ist, dass man durch einen Schieber
+        einstellen kann, ob die Uhr am Ende des Zeitfensters einen Signalton von
+        sich gibt oder nicht (dies ist keine bezahlte Werbung, nur eine
+        persönliche Empfehlung). TimeTimer sind auch als App verfügbar.
+      </p>
+      <p>
+        TimeTimer können nicht nur bei Betreuungswechseln hilfreich sein,
+        sondern insgesamt bei Übergängen (z.B. wenn wir in zehn Minuten von
+        Zuhause loswollen) oder Wartzeiten (z.B. wenn wir fünfzehn Minuten Zeit
+        zum Telefonieren brauchen).
+      </p>
+      <p>
+        Natürlich können wir die Zeit für unser Kind auch auf andere Weisen
+        visualisieren, z.B. mit klassischen Sanduhren. Eine Idee zum
+        Selberbasteln ist die folgende: Wir können die beiden Zeiger einer Uhr
+        mit zwei unterschiedlichen Farben einfärben. Zusätzlich färben wir zwei
+        Klammern mit den gleichen zwei Farben ein. Um einen bestimmten Zeitpunkt
+        zu visualisieren können wir nun die Klammern an den Stellen an der Uhr
+        anbringen, auf die die Zeiger zum bestimmten Zeitpunkt zeigen werden.
+      </p>
+      <g-image
+        alt="Uhr mit farbigen Zeigern"
+        class="img"
+        src="~/assets/imgs/uhr.jpg"
+      />
+      <br /><br />
+      <h2>Idee Nummer 2: Eine Aktivitätenliste schreiben</h2>
+      <p>
+        Vor kurzem haben wir entschieden, dass unsere Tochter eine Nacht und
+        einen Tag bei ihrer Oma «in die Ferien» geht. Sie kennt die Oma gut und
+        hat eine gute Beziehung. Als wir unseren etwas längeren Abschied
+        ankündeten, gab es dennoch einige Tränen. Völlig verständlich und in
+        Ordnung. Wir haben diese Tränen mit verständnisvollen Worten («Du bist
+        gerade traurig, dass wir gleich gehen, nicht wahr?») und vor allem mit
+        ganz viel Nähe und Zuneigung begleitet. Nachdem die Tränen etwas
+        versiegt waren, haben wir unserer Tochter vorgeschlagen, mit Oma eine
+        Liste zu erstellen, mit allem, was sie in diesen kurzen Ferien gemeinsam
+        tun könnten. Unsere Tochter hat sich sofort darauf eingelassen und ihrer
+        Oma einige Ideen zum Aufschreiben diktiert (z.B. im Sandkasten spielen).
+        Ihre Oma hat weitere Ideen vorgeschlagen, so dass eine ziemlich lange
+        Liste entstand. Natürlich kann unsere Tochter noch nicht lesen, deswegen
+        hat die Oma die Liste mit kleinen Zeichnungen ergänzt/visualisiert.
+        Danach haben die beiden vereinbart, mit welchem Listenpunkt sie beginnen
+        wollen, sobald wir Eltern weg sein würden. Beim Abschied gab es nochmals
+        ein kleines Tränchen, doch noch während wir ins Auto stiegen, rannte
+        meine Tochter zur Liste und wollte unbedingt mit all den tollen
+        Aktivitäten beginnen, die darauf standen.
+      </p>
+      <g-image
+        alt="Aktivitätenliste"
+        class="img"
+        src="~/assets/imgs/liste.jpg"
+      />
+      <p>
+        Wenn ein Betreuungswechsel bzw. ein Abschied bevorsteht, sind die
+        schwierigen Gefühle in Bezug auf den Übergang und die Trennung für ein
+        Kind manchmal überwältigend. Es wird davon so stark vereinnahmt, dass es
+        nichts anderes mehr denken oder fühlen kann. Hier kann es helfen, den
+        Blick des Kindes etwas zu weiten und auf die schönen Aktivitäten, die
+        ihm nach dem Übergang bevorstehen, zu lenken. Entweder bevor das Kind
+        von den schwierigen Gefühlen überrumpelt wird oder nachdem es sich
+        wieder etwas beruhigen konnte.
+      </p>
+      <p>
+        Dazu können wir das Kind fragen, was es denn nach dem Betreuungswechsel
+        gerne mit der neuen Betreuungsperson unternehmen möchte oder worauf es
+        sich denn freue. Dann können wir das, was uns möglich erscheint, auf
+        eine Liste schreiben (z.B. Seifenblasen machen, ein Buch anschauen, eine
+        Banane essen, auf den Spielplatz gehen…). Natürlich können wir auch
+        eigene Vorschläge einbringen. Die Liste können wir entweder vor unserem
+        Abschied mit dem Kind erstellen und sie dann der neuen Betreuungsperson
+        übergeben oder wir können unser Kind und die neue Betreuungsperson dazu
+        ermuntern, gemeinsam eine Liste zu erstellen, während wir noch anwesend
+        sind.
+      </p>
+      <p>
+        Es spielt keine Rolle, ob das Kind schon lesen kann oder noch nicht.
+        Allenfalls können wir die Liste durch kleine Zeichnungen ergänzen, um
+        sie für das Kind zu visualisieren. rsagensängste zu entwickeln.
+      </p>
+      <p>
+        Das gemeinsame Erstellen einer solchen Liste hat verschiedene Vorteile:
+      </p>
+      <ul>
+        <li>
+          Das Kind kann Vorfreude auf die neue Betreuungssituation entwickeln,
+          indem es seinen Fokus auf schöne Aktivitäten lenkt, die nach dem
+          Übergang möglich sind.
+        </li>
+        <li>
+          Das Kind erlebt, dass seine Ideen ernst genommen werden. Es spielt
+          keine Rolle, ob alle Listenpunkte abgehakt werden können. Allein das
+          Aufschreiben davon, was alles möglich wäre, kann einem Kind helfen,
+          sich gesehen zu fühlen.
+        </li>
+        <li>
+          Die Liste schafft eine Orientierung. Sie kann gut sichtbar für das
+          Kind und für die Betreuungsperson aufgehängt und immer wieder
+          konsultiert werden.
+        </li>
+      </ul>
+      <p>
+        Manchmal erstellen wir auch vor einer Trennung eine Liste für die Zeit
+        nach dem Widersehen. Wir fragen z.B. unsere Tochter am Morgen, was wir
+        am Nachmittag nach der Kita noch gemeinsam unternehmen könnten und ob
+        sie eine Liste erstellen wolle. Wenn wir dann unsere Tochter von der
+        Kita abholen, bringen wir die Liste mit und schauen, worauf wir noch
+        Lust haben.
+      </p>
+      <br />
+      <h2>Idee Nummer drei: Eine visualisierte Wochenübersicht erstellen</h2>
+      <p>
+        Eine Wochenübersicht der verschiedenen Betreuungssituationen kann einem
+        Kind Orientierung und Struktur geben und ihm dadurch Übergänge
+        erleichtern. Dazu kann man z.B. ein Whiteboard, eine Wandtafel oder eine
+        Pinwand in sieben Spalten (für jeden Wochentag) und in drei Zeilen
+        (Vormittag, Nachmittag, Abend) einteilen. In den einzelnen Feldern
+        visualisiert man nun die Betreuungssituation. Dazu kann man z.B.
+        Fotomagnete mit Fotos der verschiedenen Betreuungspersonen herstellen
+        lassen. Man kann Fotos ausdrucken oder Kärtchen zeichnen.
+      </p>
+      <p>
+        Den aktuellen Wochentag kann man mit einer Klammer/mit Farbe/mit einem
+        Magnet oder ähnlichem markieren.
+      </p>
+      <g-image
+        alt="Wochenplan"
+        class="img"
+        src="~/assets/imgs/wochenplan.jpg"
+      />
+      <p>
+        Zusätzlich kann man Magnete/Kärtchen mit wiederkehrenden Aktivitäten
+        (z.B. Babyschwimmen, Krabbelgruppe, Einkaufen, Spielplatz usw.)
+        erstellen und so auf auch gleich visualisieren, was auf dem
+        Tagesprogramm steht.
+      </p>
+    </section>
+    <section>
+      <h2>Mehr zum Thema</h2>
+      <ul class="post-list">
+        <li>
+          <g-link
+            class="post-list__post"
+            to="/blog/warum-erziehung-nicht-funktionieren-muss/"
+          >
+            <g-image
+              class="post-list__img"
+              src="~/assets/imgs/schlecht-erzogen-gut-erzogen2.png"
+            />
+            <div class="post-list__content">
+              <h2 class="post-list__title">
+                Warum Erziehungsstrategien nicht funktionieren müssen
+                <div class="post-list__date-and-author">
+                  26. März 2021 – Eva Fleer
+                </div>
+              </h2>
+              Wie sinnvoll eine Erziehungsstrategie ist, lässt sich nicht am
+              Verhalten, nicht am Lernerfolg und auch nicht an der Zufriedenheit
+              eines Kindes messen.
+            </div>
+          </g-link>
+        </li>
+        <li>
+          <g-link
+            class="post-list__post"
+            to="/blog/nein-sagen-drei-strategien/"
+          >
+            <g-image
+              class="post-list__img"
+              src="~/assets/imgs/Baby-spielt-mit-Flasche2.png"
+            />
+            <div class="post-list__content">
+              <h2 class="post-list__title">
+                "Nein" sagen, aber wie? Drei Strategien, die alle Eltern kennen
+                sollten!
+                <div class="post-list__date-and-author">
+                  07. März 2021 – Eva Fleer
+                </div>
+              </h2>
+              "Nein" sagen und Grenzen setzen gehören zum Alltag mit Kindern
+              dazu. Wie können wir "Nein" sagen, ohne die Verbindung zum Kind zu
+              verlieren? Drei nützliche Strategien!
+            </div>
+          </g-link>
+        </li>
+        <li>
+          <g-link
+            class="post-list__post"
+            to="/blog/gleichwuerdig-oder-entwuerdigend/"
+          >
+            <g-image
+              class="post-list__img"
+              src="~/assets/imgs/gleichwuerdig-vs-entwuerdigend.png"
+            />
+            <div class="post-list__content">
+              <h2 class="post-list__title">
+                Gleichwürdig oder entwürdigend?
+                <div class="post-list__date-and-author">
+                  22. Februar 2021 – Eva Fleer
+                </div>
+              </h2>
+              Eine gleichwürdige Sprache stärkt Kinder, eine entwürdigende
+              Sprache schwächt sie. Was ist der Unterschied? Sieben konkrete
+              Beispiele!
+            </div>
+          </g-link>
+        </li>
+      </ul>
+    </section>
+    <section>
+      <EmailSubscription />
+      <BlogCtas />
+    </section>
+  </Layout>
+</template>
+
+<script>
+import BlogCtas from "~/components/BlogCtas";
+import EmailSubscription from "~/components/EmailSubscription.vue";
+import VideoVue from "~/components/VideoVue.vue";
+import { trackPageLoad } from "~/scripts/analyticsMethods";
+
+export default {
+  components: { BlogCtas, EmailSubscription, VideoVue },
+  metaInfo: {
+    meta: [
+      {
+        name: "canonical",
+        href:
+          "https://so-kommunizieren.ch/blog/drei-ideen-fuer-betreuungswechsel-und-trennungen/",
+      },
+      {
+        name: "description",
+        content:
+          "Drei Ideen, wie wir unseren Kindern Betreuungswechsel und Trennungen erleichtern können",
+      },
+    ],
+    title:
+      "Dei Ideen, um einem Kind Betreuungswechsel und Trennungen zu erleichtern",
+  },
+  mounted() {
+    trackPageLoad(this.$route.path);
+  },
+};
+</script>
